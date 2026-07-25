91835c1a7249a6a4a759e824f167b2a771161cec
karim adell <karimadell1983@gmail.com>
2026-07-25 23:02:10 +0300

    refactor(auth): remove unused email verification and self-service password reset

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -32,11 +32,6 @@
         </p>
     </header>
 
-    {{-- ================= Email Verification ================= --}}
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-        @csrf
-    </form>
-
     {{-- ================= Update Profile Form ================= --}}
     <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
         @csrf
@@ -71,31 +66,6 @@
                 autocomplete="username"
             />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            {{-- Email not verified --}}
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
-                        {{ __('Your email address is unverified.') }}
-
-                        <button
-                            form="send-verification"
-                            class="underline text-sm text-gray-600 dark:text-gray-400
-                                   hover:text-gray-900 dark:hover:text-gray-100
-                                   rounded-md focus:outline-none focus:ring-2
-                                   focus:ring-offset-2 focus:ring-indigo-500
-                                   dark:focus:ring-offset-gray-800">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
-
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
         </div>
 
         {{-- Save Button --}}
